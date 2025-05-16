cd27d8b80c2c5c7b2a4a727ed69c4c612a4ec697
Bhuwanesh16 <bhuwaneshmanivel@gmail.com>
2025-05-17 01:02:48 +0530

    files added

---
+++ aiinterview/app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from 'react'
+
+
+const RootLayout = ( { children } : { children: ReactNode}) => {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default RootLayout
